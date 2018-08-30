2483dd5c4e6cfd42e3c81dd82d205cd957aa40f9
Yilun <zyl.skysniper@gmail.com>
2018-08-30 16:38:53 -0700

    Change default rpc server to testnet
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

--- dist/nkn-wallet.js
+++ dist/nkn-wallet.js
@@ -566,7 +566,7 @@ module.exports = {
 
 module.exports = {
   assetId: '4945ca009174097e6614d306b66e1f9cb1fce586cb857729be9e1c5cc04c9c02',
-  rpcAddr: 'http://node00002.nkn.org:30003',
+  rpcAddr: 'http://testnet.nkn.org:30003',
 }
 
 },{}],7:[function(require,module,exports){
